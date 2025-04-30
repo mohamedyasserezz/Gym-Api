5c653d66132b28a96b886fa8bcc1b518be389637
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-30 19:13:39 +0300

    Like the previous

--- Gym Api/Data/AppDbContext.cs
+++ Gym Api/Data/AppDbContext.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Gym_Api.Data
 {
-	public class AppDbContext : DbContext
+	public class ApplicationDbContext : DbContext
 	{
-		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
 
 		}
