d4192457f9b3ac35e71488ce1b746fded5e1fa7c
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-04-30 19:11:49 +0300

    Add Const Classes

---
+++ Gym Api/Common/Consts/DefaultRoles.cs
@@ -0,0 +1,23 @@
+﻿namespace Gym_Api.Common.Consts
+{
+    public static class DefaultRoles
+    {
+        public const string Admin = nameof(Admin);
+        public const string AdminRoleId = "92b75286-d8f8-4061-9995-e6e23ccdee94";
+        public const string AdminRoleConcurrencyStamp = "f51e5a91-bced-49c2-8b86-c2e170c0846c";
+
+
+        public const string Coach = nameof(Coach);
+        public const string CoachRoleId = "9eaa03df-8e4f-4161-85de-0f6e5e30bfd4";
+        public const string CoachRoleConcurrencyStamp = "5ee6bc12-5cb0-4304-91e7-6a00744e042a";
+
+        public const string User = nameof(User);
+        public const string UserRoleId = "ba9a059b-9827-4275-bc7b-c9292a3ee37f";
+        public const string UserRoleConcurrencyStamp = "b453957e-c4d2-4f68-9fd2-81d091b91702";
+
+
+        public const string Trainee = nameof(Trainee);
+        public const string TraineeRoleId = "bc0ac5e8-9715-4e19-b88d-421cb89c44a6";
+        public const string TraineeRoleConcurrencyStamp = "683789c4-7a04-4f5b-8880-474488af37ad";
+    }
+}
